6d660717b1e8ad8c3c3210fe42ea38848f537073
Mustifix <131684539+Mustifix@users.noreply.github.com>
2023-12-02 11:49:15 +0300

    Fix index.js

--- index.js
+++ index.js
@@ -150,8 +150,8 @@ client.on('interactionCreate', async interaction => {
 
         let sistem = mustifixdb.get(`uptimeSistemi_${interaction.guild.id}`)
         if (!sistem) return;
-        let ozelrol = sistem.rol
-        let log = sistem.log
+        let ozelrol = "1174688571802787912"//sistem.rol
+        let log = "1174646342241488912"//sistem.log
         if (!log) return;
         var logChannel = client.channels.cache.get(log)
 
@@ -257,7 +257,7 @@ client.on('interactionCreate', async interaction => {
 
         const logEmbed = new EmbedBuilder()
             .setColor("ff0000")
-            .setDescription(`<@${interaction.user.id}> adlı kullanıcı sistemden bir **link sildi!**\n\n Sistemde kalan link sayısı **${links.length}** `)
+            .setDescription(`<@${interaction.user.id}> adlı kullanıcı sistemden bir **link sildi!**\n\n Sistemde kalan link sayısı **'.length'** \n\n :link: Link:`)
 			.setImage("https://cdn.discordapp.com/attachments/1105524120805969952/1170422555811795065/standard_1.gif?ex=6558fbf2&is=654686f2&hm=47ee81e75b57ee46879e430905d54f17625daa673ab016783cbefcd14cb16a11&")
         logChannel.send({ embeds: [logEmbed] }).catch(e => { })
     }
